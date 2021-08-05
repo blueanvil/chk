e25521e0cb0cda92022668529c847a4d31bb9a0d
Cosmin Marginean <cos@hey.com>
2021-08-05 16:51:25 +0100

    Parallelism capabilities

--- src/main/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClient.kt
+++ src/main/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClient.kt
@@ -5,6 +5,10 @@ import io.github.bucket4j.Bandwidth
 import io.github.bucket4j.BlockingBucket
 import io.github.bucket4j.Bucket4j
 import java.time.Duration
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Cosmin Marginean
@@ -16,12 +20,33 @@ class CompaniesHouseRestClient(private val apiKey: String,
         return ApiRequest(apiKey, bucket, resource)
     }
 
-    fun allResults(pagedResource: String, itemsPerPage: Int = RESULTS_PER_PAGE): Sequence<JsonObject> {
+    fun allResults(pagedResource: String): Sequence<JsonObject> {
         return allResults { startIndex ->
-            val appendChar = if (pagedResource.contains("?")) "&" else "?"
-            val response = request(pagedResource + "${appendChar}items_per_page=${itemsPerPage}&start_index=${startIndex}").get()
-            PagedResponse(response)
+            pagedGet(pagedResource, startIndex)
         }
+    }
+
+    fun allResults(pagedResource: String,
+                   threadPool: ExecutorService,
+                   handleItems: (List<JsonObject>) -> Unit) {
+        allResultsParallel(threadPool, handleItems) { startIndex ->
+            pagedGet(pagedResource, startIndex)
+        }
+    }
+
+    fun allResults(pagedResource: String,
+                   threads: Int,
+                   handleItems: (List<JsonObject>) -> Unit) {
+        val threadPool = Executors.newFixedThreadPool(threads)
+        allResults(pagedResource, threadPool, handleItems)
+        threadPool.shutdown()
+        threadPool.awaitTermination(1, TimeUnit.MINUTES)
+    }
+
+    private fun pagedGet(pagedResource: String, startIndex: Int): PagedResponse {
+        val appendChar = if (pagedResource.contains("?")) "&" else "?"
+        val response = request(pagedResource + "${appendChar}items_per_page=${RESULTS_PER_PAGE}&start_index=${startIndex}").get()
+        return PagedResponse(response)
     }
 
     private fun allResults(fetchPage: (Int) -> PagedResponse): Sequence<JsonObject> {
@@ -45,6 +70,26 @@ class CompaniesHouseRestClient(private val apiKey: String,
                 currentElement
             }
         }
+    }
+
+    private fun allResultsParallel(threadPool: ExecutorService,
+                                   handleItems: (List<JsonObject>) -> Unit,
+                                   fetchPage: (Int) -> PagedResponse) {
+
+        val futures = mutableListOf<Future<*>>()
+        val firstResponse = fetchPage(0)
+        if (firstResponse.totalResults > 0) {
+            threadPool.submit { handleItems(firstResponse.items) }
+            val pageCount = firstResponse.totalResults / ChJson.RESULTS_PER_PAGE + 1
+            (1 until pageCount).map { pageIndex ->
+                val future = threadPool.submit {
+                    val items = fetchPage(pageIndex * ChJson.RESULTS_PER_PAGE).items
+                    handleItems(items)
+                }
+                futures.add(future)
+            }
+        }
+        futures.forEach { it.get() }
     }
 
     companion object {
--- src/test/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClientTest.kt
+++ src/test/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClientTest.kt
@@ -5,6 +5,7 @@ import org.testng.Assert.assertEquals
 import org.testng.Assert.assertTrue
 import org.testng.annotations.Test
 import java.io.File
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * @author Cosmin Marginean
@@ -20,5 +21,18 @@ class CompaniesHouseRestClientTest {
         assertTrue(client.allResults("/search/companies?q=powders").count() > 60)
         assertTrue(client.allResults("/officers/tZDPlH3KiSJmoRHNwFx0OKsLF64/appointments").count() >= 90)
         assertEquals(client.request("/company/1234").get().code, 404)
+    }
+
+    @Test
+    fun parallelism() {
+        val companyNumber = "02685120"
+        val count = client.allResults("/company/$companyNumber/officers").count()
+        println("Found $count officers with regular fetch")
+        val parallelCount = AtomicLong(0)
+        client.allResults("/company/$companyNumber/officers", 4) { appts ->
+            appts.forEach { parallelCount.incrementAndGet() }
+        }
+        println("Found ${parallelCount.get()} officers with parallel fetch")
+        assertEquals(count.toLong(), parallelCount.get())
     }
 }
