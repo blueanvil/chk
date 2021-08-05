4f995e98585d710e38c605e62557805da3a1a71c
Cosmin Marginean <cos@hey.com>
2021-08-05 16:30:59 +0100

    Cleanup

--- src/test/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClientTest.kt
+++ src/test/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClientTest.kt
@@ -2,16 +2,19 @@ package com.blueanvil.chk.lowlevel
 
 import org.testng.Assert.assertTrue
 import org.testng.annotations.Test
+import java.io.File
 
 /**
  * @author Cosmin Marginean
  */
 class CompaniesHouseRestClientTest {
 
-    private val apiKey = System.getProperty("chkTestKey")
+    private val apiKey = File("./companies-house-test-api-key.txt").readText().trim()
+    private val client = CompaniesHouseRestClient(apiKey)
 
     @Test
-    fun searchSequence() {
-        assertTrue(CompaniesHouseRestClient(apiKey).allResults("/search/companies?q=powders").count() > 60)
+    fun basicRestFunctions() {
+        assertTrue(client.allResults("/search/companies?q=powders").count() > 60)
+        assertTrue(client.allResults("/officers/tZDPlH3KiSJmoRHNwFx0OKsLF64/appointments").count() >= 90)
     }
 }
