b2280c04d46d49c194c87d9f9c33c26f7f1d345b
Cosmin Marginean <cos@hey.com>
2021-08-06 11:26:47 +0100

    Add test for clustered buckets

--- src/test/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClientTest.kt
+++ src/test/kotlin/com/blueanvil/chk/lowlevel/CompaniesHouseRestClientTest.kt
@@ -1,11 +1,19 @@
 package com.blueanvil.chk.lowlevel
 
 import com.blueanvil.chk.text
+import com.hazelcast.map.IMap
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket4j
+import io.github.bucket4j.grid.GridBucketState
+import io.github.bucket4j.grid.RecoveryStrategy
+import io.github.bucket4j.grid.hazelcast.Hazelcast
 import org.testng.Assert.assertEquals
 import org.testng.Assert.assertTrue
 import org.testng.annotations.Test
 import java.io.File
+import java.time.Duration
 import java.util.concurrent.atomic.AtomicLong
+
 
 /**
  * @author Cosmin Marginean
@@ -37,5 +45,24 @@ class CompaniesHouseRestClientTest {
         println("Found ${parallelCount.get()} officers with parallel fetch")
 
         assertEquals(count.toLong(), parallelCount.get())
+    }
+
+    @Test
+    fun clusteredBucket() {
+        val map: IMap<String, GridBucketState> = com.hazelcast.core.Hazelcast.newHazelcastInstance().getMap("bucketmap")
+        val bandwidth = Bandwidth.simple(590, Duration.ofMinutes(5))
+        val bucket = Bucket4j.extension(Hazelcast::class.java)
+                .builder()
+                .addLimit(bandwidth)
+                .build(map, "key", RecoveryStrategy.RECONSTRUCT)
+        val clusterClient = CompaniesHouseRestClient(apiKey, bucket.asScheduler())
+
+
+        val pagedResource = "/company/02685120/officers"
+        val parallelCount = AtomicLong(0)
+        clusterClient.allResults(pagedResource, 4) { appts ->
+            parallelCount.addAndGet(appts.size.toLong())
+        }
+        assertTrue(parallelCount.get() > 100)
     }
 }
