93e8fee88a8cb6f6165bcefc0a8418a76c57659f
Cosmin Marginean <cos@hey.com>
2021-08-10 09:36:11 +0100

    Company number should not be null

---
+++ src/main/kotlin/com/blueanvil/chk/ChkConst.kt
@@ -0,0 +1,9 @@
+package com.blueanvil.chk
+
+/**
+ * @author Cosmin Marginean
+ */
+object ChkConst {
+
+    const val COMPANY_NUMBER_UNKNOWN = "UNKNOWN"
+}
--- src/main/kotlin/com/blueanvil/chk/model/CompanyInfo.kt
+++ src/main/kotlin/com/blueanvil/chk/model/CompanyInfo.kt
@@ -1,16 +1,13 @@
 package com.blueanvil.chk.model
 
 import com.beust.klaxon.JsonObject
-import com.blueanvil.chk.ChJson
-import com.blueanvil.chk.fixCompanyNumber
-import com.blueanvil.chk.name
-import com.blueanvil.chk.officerId
+import com.blueanvil.chk.*
 
 /**
  * @author Cosmin Marginean
  */
 data class CompanyInfo(val name: String,
-                       val companyNumber: String?,
+                       val companyNumber: String,
                        val companyStatus: String?,
                        val officerId: String?,
                        val numberOfAppointments: Int,
@@ -20,7 +17,7 @@ data class CompanyInfo(val name: String,
 
     constructor(record: JsonObject) : this(
             name = record.name(),
-            companyNumber = record.string(ChJson.COMPANY_NUMBER).fixCompanyNumber(),
+            companyNumber = record.string(ChJson.COMPANY_NUMBER)?.fixCompanyNumber() ?: ChkConst.COMPANY_NUMBER_UNKNOWN,
             companyStatus = record.string(ChJson.COMPANY_STATUS),
             officerId = record.officerId(),
             numberOfAppointments = record.int(ChJson.APPT_COUNT) ?: 0,
